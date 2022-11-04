156bee77c5402494aa3d3ee56a2a633319e3da8b
yusrilihzaM <mohyusrilihzamaulana@gmail.com>
2022-11-04 23:24:38 +0700

    1. Upgrade version angular,

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import swal from 'sweetalert2';
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
@@ -9,6 +11,10 @@ export class AppComponent implements OnInit {
   isHidden: boolean = true;
   formGroup;
   rawDataBase64;
+  sorrySwallError = 'Sorry';
+  textSwallError = 'You must enter the correct Base64 script!';
+  base64Steam = 'data:application/octet-stream;base64,';
+  filename = 'result.xls';
 
   constructor(private formBuilder: FormBuilder) {
     this.formGroup = this.formBuilder.group({
@@ -20,7 +26,23 @@ export class AppComponent implements OnInit {
 
   onSubmit(formData) {
     this.rawDataBase64 = formData['dataBase64'];
-    this.isHidden = false;
+    if (this.rawDataBase64 <= 0) {
+      swal({
+        title: this.sorrySwallError,
+        text: this.textSwallError,
+        type: 'error',
+        showConfirmButton: true,
+        showCancelButton: false,
+      });
+    } else {
+      const linkSource = this.base64Steam + this.rawDataBase64;
+      const downloadLink = document.createElement('a');
+      const fileName = this.filename;
+
+      downloadLink.href = linkSource;
+      downloadLink.download = fileName;
+      downloadLink.click();
+    }
   }
 
   onKeyTextArea() {
